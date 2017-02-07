cdc2798963ce2f841a00fde55fd975416cce0bc0
lihongsheng <lihongsheng@leyue100.com>
2017-02-08 00:22:43 +0800

    lihongsheng

--- customer/Lib/PcntlModel.php
+++ customer/Lib/PcntlModel.php
@@ -12,16 +12,45 @@ namespace customer\Lib;
 
 class PcntlModel
 {
+
+    /**
+     * 启动状态
+     *
+     * @var int
+     */
+    const STATUS_STARTING = 1;
+
+    /**
+     * 运行状态
+     *
+     * @var int
+     */
+    const STATUS_RUNNING = 2;
+
+    /**
+     * 关闭状态
+     *
+     * @var int
+     */
+    const STATUS_SHUTDOWN = 4;
+
+    /**
+     * 重启状态
+     *
+     * @var int
+     */
+    const STATUS_RELOADING = 8;
+
     /**
      * 是否守护进程
      * @var bool
      */
-    private static $Daemonize = false;
+    private $Daemonize = false;
 
     /**最多开启的工作进程数
      * @var int
      */
-    private static $MaxSize;
+    private $MaxSize;
 
     /**masterId 主进程ID
      * $var int
@@ -55,9 +84,12 @@ class PcntlModel
 
     public function __construct()
     {
-        PcntlModel::setDaemonize();
-        PcntlModel::setMaxsize();
-        PcntlModel::daemonize();
+        $this->setDaemonize();
+        $this->setMaxsize();
+        $this->daemonize();
+        $this->resetStd();
+        $this->installSignal();
+        $this->monitorWorkers();
     }
 
     /**
@@ -71,25 +103,22 @@ class PcntlModel
         $this->workRun =  $run;
     }
 
-    public static function setMaxsize()
+    public function setMaxsize()
     {
-        self::$MaxSize = Config::$MaxSize;
+        $this->MaxSize = Config::$MaxSize;
     }
 
-    public static function setDaemonize()
+    public function setDaemonize()
     {
-        self::$Daemonize = Config::$Daemonize;
+        $this->Daemonize = Config::$Daemonize;
     }
 
-    /*cli模式下运行
-	*进程守护化函数，使进程脱离当前终端控制，以便后台独立运行。
-	执行后需要通过 ps - kill 杀死此进程，
-	或者 运行 posix_getpid() 获取 当前进程ID 然后kill
-	如果不是service 服务，只是运行时间比较长，最好是在业务程序里加上 exit退出进程。
+    /**
+     * 进程守护
 	*/
-    public static function daemonize()
+    public function daemonize()
     {
-        if(!self::$Daemonize) {
+        if(!$this->Daemonize) {
             return false;
         }
         $pid = pcntl_fork();//创建子进程
@@ -119,26 +148,197 @@ class PcntlModel
         while ($i<PcntlModel::$MaxSize) {
             $i++;
             $pid = pcntl_fork();
-            if($pid == 0) {
-                $run = $this->workRun;
-                $this->work->$run();
-                $pid = posix_getpid();
-                $this->works[$pid] = $pid;
-            }else if ($pid == -1) {
+            if($pid === 0) {
+               $this->forkWork();
+            } else if ($pid === -1) {
                 if($i>0) {
                     $i--;
                 }
             }
+            //子进程ID
+            $this->works[$pid] = $pid;
+        }
+        //主进程ID
+        $this->MasterId = posix_getpid();
 
-        }
-        /*
-         * 主进程工作ID
-         */
-        if(!empty($this->masterWork)) {
-            call_user_func($this->masterWork, $this);
-        }
-        //$p = pcntl_wait($status);
     }
+
+    /**
+     * 子进程工作启动
+     */
+    private function forkWork() {
+        $run = $this->workRun;
+        $this->work->$run();
+        $pid = posix_getpid();
+        $this->reinstallSignal();
+    }
+
+    protected function forkOne() {
+        $pid = pcntl_fork();
+        if($pid === -1){
+            throw new Exception("forkOneWorker fail ON LINE ".__LINE__);
+        } else if($pid === 0) {
+            $this->forkWork();
+        }
+        $this->works[$pid] = $pid;
+    }
+
+    /**
+     * 重置输出
+     * @throws Exception
+     */
+    protected function resetStd()
+    {
+        if (!$this->Daemonize) {
+            return;
+        }
+        global $STDOUT, $STDERR;
+        $handle = fopen($this->StdoutFile, "a");
+        if ($handle) {
+            unset($handle);
+            @fclose(STDOUT);
+            @fclose(STDERR);
+            $STDOUT = fopen($this->StdoutFile, "a");
+            $STDERR = fopen($this->StdoutFile, "a");
+        } else {
+            throw new Exception('can not open stdoutFile ' . $this->StdoutFile);
+        }
+    }
+
+
+
+    /**
+     * 注册信号处理函数
+     *
+     * @return void
+     */
+    protected function installSignal()
+    {
+        // stop
+        pcntl_signal(SIGINT, array($this, 'signalHandler'), false);
+        // reload
+        pcntl_signal(SIGUSR1, array($this, 'signalHandler'), false);
+        // status
+        pcntl_signal(SIGUSR2, array($this, 'signalHandler'), false);
+        // ignore
+        pcntl_signal(SIGPIPE, SIG_IGN, false);
+    }
+
+
+    protected function reinstallSignal()
+    {
+        // uninstall stop signal handler
+        pcntl_signal(SIGINT, SIG_IGN, false);
+        // uninstall reload signal handler
+        pcntl_signal(SIGUSR1, SIG_IGN, false);
+        // uninstall  status signal handler
+        pcntl_signal(SIGUSR2, SIG_IGN, false);
+    }
+
+    /**
+     * 信号处理
+     *
+     * @param int $signal
+     */
+    public function signalHandler($signal)
+    {
+        switch ($signal) {
+            // Stop.
+            case SIGINT:
+                $this->stopAll();
+                break;
+            // Reload.
+            case SIGUSR1:
+                $this->reload();
+                break;
+            // Show status.
+            case SIGUSR2:
+                $this->writeStatisticsToStatusFile();
+                break;
+        }
+    }
+
+    protected function reload()
+    {
+
+    }
+
+    protected function writeToStatusFile()
+    {
+
+    }
+
+
+    /**
+     * Stop.
+     *
+     * @return void
+     */
+    public function stopAll()
+    {
+        $this->Status = PcntlModel::STATUS_SHUTDOWN;
+
+        if(!empty($this->works)) {
+            foreach($this->works as $val) {
+                posix_kill($val,SIGINT);
+            }
+        }
+        $this->exitAndClearAll();
+
+    }
+
+
+
+
+
+    public function monitorWorkers()
+    {
+
+        while (1) {
+            //等待信号处理器
+            pcntl_signal_dispatch();
+            $status = 0;
+            $pid    = pcntl_wait($status, WUNTRACED); //子进程已经退出并且其状态未报告时返回
+
+            pcntl_signal_dispatch();
+            // 如果一个子进程已经退出
+            if ($pid > 0) {
+                if ($this->Status !== PcntlModel::STATUS_SHUTDOWN) {
+                    if($this->works[$pid]) {
+                        unset($this->works[$pid]);
+                        $this->forkOne();
+                        if ($status !== 0) {
+                            $this->log("worker[:$pid] exit with status $status");
+                        }
+                    }
+                } else {
+                    $this->exitAndClearAll();
+                }
+            } else {
+                // 主进程shutdown并且子进程全部exit
+                if ($this->Status === PcntlModel::STATUS_SHUTDOWN && empty($this->works)) {
+                    self::exitAndClearAll();
+                }
+            }
+            //主进程工作ID
+            if(!empty($this->masterWork)) {
+                call_user_func($this->masterWork);
+            }
+        }
+
+    }
+
+    /**
+     *
+     */
+    protected static function exitAndClearAll()
+    {
+        //清除工作
+        exit(0);
+    }
+
+
+
 
 
 }
